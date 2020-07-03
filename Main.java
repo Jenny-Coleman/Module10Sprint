d3eab60ec7d372d92129bbaa2e667f6bf93c3723
Jenny-Coleman <62373472+Jenny-Coleman@users.noreply.github.com>
2020-07-03 03:51:02 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+
+public class Main {
+    public static Connection con;
+
+    public static void main(String[] args) {
+    Main pro = new Main();
+    pro.createConnection();
+    
+    new AdminLogin().setVisible(true);
+    }
+
+    void createConnection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schooldb", "root", "root");
+        } catch (ClassNotFoundException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
